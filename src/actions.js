ab82f2f8b659daec719d5d19b5d735ad9c886317
Josiane Dufour <josiane@Josianes-MacBook-Pro.local>
2019-05-13 17:43:29 +0100

    did some stuff???

---
+++ src/actions.js
@@ -0,0 +1,14 @@
+import AppDispatcher from "./Application";
+
+export const updateNumberOfPeople = value => {
+  const action = { type: "UPDATE_NUMBER_OF_PEOPLE", value };
+  AppDispatcher.dispatch(action);
+};
+
+export const updateSlicesPerPerson = value => {
+  AppDispatcher.dispatch({ type: "UPDATE_SLICES_PER_PERSON", value });
+};
+
+export const reset = () => {
+  AppDispatcher.dispatch({ type: "RESET" });
+};
